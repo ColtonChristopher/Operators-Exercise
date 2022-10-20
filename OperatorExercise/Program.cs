7a5ea71038835ab39d0ba512e557c59bc8c92a85
Colton Christopher <2tontide@gmail.com>
2022-10-20 13:15:10 -0500

    completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,7 +1,6 @@
-﻿namespace OperatorExercise
+﻿
+public class Program
 {
-    public class Program
-    {
         static void Main(string[] args)
         {
             int a = 17;
@@ -9,9 +8,15 @@
             int quotient = a / b;
             int remainder = a % b;
             if (a == 17 && b == 4)
-           
 
-            Console.WriteLine($"17/4 is 4 remainder 1");
+
+                Console.WriteLine($"17/4 is 4 remainder 1");
         }
+       static void AreaOfCircle(string[] args)
+    {
+        Console.WriteLine("What is the radius of your circle:");
+        var radius = double.Parse(Console.ReadLine());
+        double Area = Math.PI * radius * radius;
+        Console.WriteLine("Area of circle: " + Area);
+        Console.ReadKey();
     }
-}
